fc384f57931255869ca70937bc613377de19eb1a
Tony <tonyaleynikov@gmail.com>
2022-09-07 02:12:37 +0300

    Добавил проверку сесии перед экшенами. Добавил метод current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :check_login
+  private
+
+  def current_user
+    @currenr_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def check_login
+    unless session[:user_id].present?
+      redirect_to root_path
+    end
+  end
 end
