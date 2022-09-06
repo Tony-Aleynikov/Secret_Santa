300e9aedee86c6788ef53268dc68cc809697fc34
Tony <tonyaleynikov@gmail.com>
2022-09-07 02:13:10 +0300

    Добавил экшены в Евент контроллер new create destroy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    event_params = params.require(:event).permit(:name, :password)
+    current_user.events.create(event_params)
+
+    redirect_to user_path(current_user.id)
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+
+    redirect_to user_path(current_user.id) #TODO страница эвентов
+  end
+
 end
