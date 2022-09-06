831cd166fb637fde92a8d90d85c4c5d48018f922
Tony <tonyaleynikov@gmail.com>
2022-09-07 02:14:22 +0300

    Дописал экшены в session controller (new create destroy) + skip before actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  skip_before_action :check_login, only: %i[new create]
+  def new
+  end
+
+  def create
+    user_params = params.require(:session)
+    user = User.find_by(name: user_params[:name])
+    if user.present?
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
 end
