1c48cacd481478401b401df0564a5e89f10b0248
Tony <tonyaleynikov@gmail.com>
2022-09-14 17:28:36 +0300

    Добавлены роуты для эвентов, рилейшенов, создания сообщения, стата эвентов, чатов

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root "info#index"
 
+  resources :events, only: %i[new create show destroy]
+  resources :relations, only: %i[new create]
   resources :users, except: :index
-  resources :events, only: %i[new create destroy]
+  resources :messages, only: :create
   resource :session, only: %i[new create destroy]
 
   get 'users/:id/events', to: 'users#events'
-
-
+  get 'events/:id/start', to: 'events#start'
+  get 'events/:id/chat_for_santa', to: 'events#chat_for_santa'
+  get 'events/:id/chat_with_santa', to: 'events#chat_with_santa'
 end
